e3fc489c1ff5408ddd5ed05c6405248d9872161a
GermainR24 <germainronald23@gmail.com>
2024-09-06 09:51:28 -0500

    actualizar el app.js

--- Actividad1/devops-practice/src/app.js
+++ Actividad1/devops-practice/src/app.js
@@ -6,12 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Puerto donde correrá el servidor
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
 // Exporta la aplicación para que pueda ser utilizada en las pruebas
 module.exports = app;
 
+if (require.main === module) {
+    const port = process.env.PORT || 0; 
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
